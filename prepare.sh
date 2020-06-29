ee8fb8b988578d594709ea3ebdf4204ad6390a7a
Emilio Giordano <emilio.giordano@n5now.com>
2020-06-29 07:32:23 -0300

    added  variables

--- prepare.sh
+++ prepare.sh
@@ -9,10 +9,12 @@ sed -i 's/name =.*/name = '"$NAME"'/' variables.tfvars
 
 # Configure AWS
 aws configure
+
 # Generate keys
 ssh-keygen -t rsa -C $NAME -f ~/.ssh/$NAME.pem
 chmod 400  ~/.ssh/$NAME.pem
 sed -i 's/private_key_path =.*/private_key_path = ~\/.ssh\/'"$NAME"'.pem/' variables.tfvars
+
 # Extract pub key
 ssh-keygen -y -f ~/.ssh/$NAME.pem > ~/.ssh/$NAME.pub
 chmod 444 ~/.ssh/$NAME.pub
--- variables.tfvars
+++ variables.tfvars
@@ -1,11 +1,11 @@
-name = pepe8
+name = testone1
 
 region = "us-east-1"
 
 instance_type = "t2.micro"
 
-key_name = pepe8
+key_name = testone1
 
-public_key_path = ~/.ssh/pepe8.pub
+public_key_path = ~/.ssh/testone1.pub
 
-private_key_path = ~/.ssh/pepe8.pem
+private_key_path = ~/.ssh/testone1.pem
